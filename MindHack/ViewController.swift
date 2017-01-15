f7423a02de510ded53985b8572dcbeee395531b4
Bryan <bryanye97@gmail.com>
2017-01-15 13:58:38 +1100

    Initial Commit

---
+++ MindHack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MindHack
+//
+//  Created by Bryan Ye on 15/1/17.
+//  Copyright © 2017 Bryan Ye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
